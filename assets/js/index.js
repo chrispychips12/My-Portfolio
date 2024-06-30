8c797518ab7696164d14052d9752ab6ca72adf8f
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-06-30 12:34:54 +1000

    removed duplicate function

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,12 +5,6 @@ document.getElementById('toggle-mode').addEventListener('click', function() {
     });
 });
 
-document.getElementById('toggle-mode').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
-    document.querySelectorAll('header, button, a, nav, p, main, .featured-project, .card, #contact, input, textarea, footer').forEach(function(element) {
-        element.classList.toggle('dark-mode');
-    });
-});
 
 document.getElementById('play-game').addEventListener('click', function() {
     let userChoice = prompt("Rock, Paper, or Scissors?");
