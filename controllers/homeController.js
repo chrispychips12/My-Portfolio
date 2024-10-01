542ae33130619ba1e16a75cc404d0a0b949d542e
Christopher <94519893+chrispychips12@users.noreply.github.com>
2024-10-01 12:18:00 +1000

    Update homeController.js

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { myProjects, groupProjects, techLogos } = require('../models/projectModel');
 
 const journeyItems = [
     {
@@ -47,6 +46,40 @@ const journeyItems = [
     }
 ];
 
+const myProjects = [
+  { name: 'Prework Study Guide', description: 'A study guide for USYD prework', url: 'https://github.com/chrispychips12/prework-study-guide' },
+  { name: 'Mini Project 1', description: 'First mini project', url: 'https://github.com/chrispychips12/mini-project-1' },
+  { name: 'Advanced CSS Wireframe', description: 'Wireframe using advanced CSS', url: 'https://github.com/chrispychips12/Advanced-CSS-Wireframe-1' },
+  { name: 'USYD Challenge Week 1', description: 'First week challenge for USYD', url: 'https://github.com/chrispychips12/USYD-Challenge-week1' },
+  { name: 'Pay Roll Tracker', description: 'Application to track payroll', url: 'https://github.com/chrispychips12/Pay-Roll-Tracker' },
+  { name: 'Personal Blog Template', description: 'Template for a personal blog', url: 'https://github.com/chrispychips12/Personal-Blog-Template' },
+  { name: 'Personal Task Board', description: 'A board to manage personal tasks', url: 'https://github.com/chrispychips12/personal-task-board' },
+  { name: 'Weather Dashboard', description: 'Dashboard to display weather information', url: 'https://github.com/chrispychips12/weather-dashboard' },
+  { name: 'ReadMe Generator', description: 'A backend ReadMe generator', url: 'https://github.com/chrispychips12/potential-enigma' },
+  { name: 'SVG Logo Generator', description: 'Tool to generate SVG logos', url: 'https://github.com/chrispychips12/svg-logo-generator' },
+  { name: 'Express Note Taker', description: 'Note-taking application using Express', url: 'https://github.com/chrispychips12/express-note-taker' },
+  { name: 'Employee Tracker', description: 'Backend application to track employees', url: 'https://github.com/chrispychips12/employee-tracker' },
+];
+
+const groupProjects = [
+  { name: 'FocusTrack', description: 'Group project for task management', url: 'https://github.com/Cbeattie97/FocusTrack' },
+  { name: 'Recipe Book', description: 'Collaborative recipe book project', url: 'https://github.com/ChrisReynolds0508/recipe-book' },
+];
+const techLogos = [
+    { name: "JavaScript", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
+    { name: "HTML5", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
+    { name: "CSS3", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
+    { name: "Handlebars", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/handlebars/handlebars-original.svg" },
+    { name: "Node.js", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
+    { name: "SQL", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" },
+    { name: "PostgreSQL", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" },
+    { name: "Sequelize", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sequelize/sequelize-original.svg" },
+    { name: "Git", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
+    { name: "GitHub", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" },
+    { name: "Bootstrap", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
+    { name: "Tailwind", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" },
+    { name: "Express", url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" }
+];
 router.get('/', (req, res) => {
     res.render('home', {
         journeyItems: journeyItems,
